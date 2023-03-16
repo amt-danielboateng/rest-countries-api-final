28e09a13a066b575472e6ffbe43b0342c726ee23
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-03-16 10:19:55 +0000

    new update

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div>
+<div className="header">
+    <div className="header-container">
+    <h2 className="logo">Where in the world?</h2>
+    
+    <div className="switch-mode"></div>
+    < i className='fas fa-moon'><h3>Dark Mode</h3></i>
+    </div>
+    
+</div>
+</div>
+    )
+}
+
+export default Header;
