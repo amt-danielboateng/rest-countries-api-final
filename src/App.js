3ac15a0dd93212ca4b1fffad10267f62b5924d19
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-03-17 12:29:50 +0000

    new update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Header from './Header';
 import './App.css';
 import Country from './Country';
@@ -7,6 +7,53 @@ import CountryDetails from './CountryDetails';
 
 function App() {
 const [darkMode, setDarkMode] = useState(false);
+const [countries, setCountries] = useState([]);
+const countriesInputRef = useRef();
+const regionRef = useRef();
+
+const noCountry = countries.status || countries.message;
+
+const fetchData = async() => {
+  const response = await fetch("https://restcountries.com/v2/all");
+  const data = await response.json();
+
+  if(data.status === 404){
+    setCountries([]);
+    return;
+  }
+
+  setCountries(data);
+}
+
+useEffect(() => {
+  try{
+    fetchData()
+  }catch(error){
+    console.log(error);
+  }
+}, [])
+
+const searchCountries = () => {
+  const searchValue = countriesInputRef.current.values;
+
+  if(searchValue.trim()){
+    const fetchSearch = async() => {
+      const response = await fetch(`https://restcountries.com/v2/name/${searchValue}`);
+      const data = response.json();
+
+      searchCountries(data);
+    }
+
+    try {
+      fetchSearch()
+    }catch(error) {
+      console.log(error);
+    }
+
+  }else {
+    fetchData();
+  }
+}
 
 const switchMode = () => {
   setDarkMode((prevState) => !prevState);
@@ -21,10 +68,10 @@ const switchMode = () => {
     <div className='app-body'>
         <div className='inputs'>
           <div className={`search-input ${darkMode ? 'darkMode': ""}`}>
-<input type="text" placeholder='search for a country...' />
+<input type="text" placeholder='search for a country...' ref={countriesInputRef} onChange={searchCountries} />
           </div>
           <div className={`select-region ${darkMode ? 'darkMode': ""}`}>
-<select>
+<select ref={regionRef}>
   <option>All</option>
   <option>Africa</option>
   <option>Americas</option>
@@ -36,7 +83,18 @@ const switchMode = () => {
         </div>
 
         <div className='countries'>
-        <Country darkMode={darkMode}/>
+        {!noCountry ? (countries.map((country, index) => (
+          <Country 
+          darkMode={darkMode}
+          key={country.alpha3Code}
+          code={country.alpha3Code}
+          name={country.name}
+          capital={country.capital}
+          population={country.population}
+          region={country.region}
+          flag={country.flag}
+          />
+  ))):(<p>No Country found...</p>) }
         </div>
       </div>
   }/>
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-const Country = ({darkMode}) => {
+const Country = ({darkMode, name, capital, population, region, flag}) => {
 
 return <div className={`country ${darkMode ? 'darkMode': ""}`}>
 <div className="flag-container">
-<img src="https://flagcdn.com/as.svg" alt="" />
+<img src={flag} alt="" />
 </div>
 
 <div className="details">
-<h3 className="name">Name</h3>
+<h3 className="name">{name}</h3>
 <p>Population: {" "}
-    <span className={`values ${darkMode ? 'darkMode': ""}`}>Test</span>
+    <span className={`values ${darkMode ? 'darkMode': ""}`}>{population}</span>
 </p>
 
 <p>Region: {" "}
-    <span className={`values ${darkMode ? 'darkMode': ""}`}>Test</span>
+    <span className={`values ${darkMode ? 'darkMode': ""}`}>{region}</span>
 </p>
 
 <p>Capital: {" "}
-    <span className={`values ${darkMode ? 'darkMode': ""}`}>Test</span>
+    <span className={`values ${darkMode ? 'darkMode': ""}`}>{capital}</span>
 </p>
 
 
